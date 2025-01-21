fd0e58a44192afa42dc9f2e24494d7725ef9d544
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-01-21 10:42:33 +0000

    Some very basic e2e tests for the Client. As it stands the information from these tests is not very useful as they very rarely raise an error even if the API response is bad.

--- tests/e2e/test_client_e2e.py
+++ tests/e2e/test_client_e2e.py
@@ -2,18 +2,59 @@ import os
 
 import pytest
 
+from uncertainty_engine.client import Client
 
+# NOTE: For these tests to run successfully, the following environment variables must be set:
+# UE_USER_EMAIL: A user email that has been registered with the Uncertainty Engine service.
+# UE_DEPLOYMENT_URL: The deployment URL for the Uncertainty Engine service.
 @pytest.mark.parametrize(
     "test_user_email, deployment_url",
     [(os.environ["UE_USER_EMAIL"], os.environ["UE_DEPLOYMENT_URL"])],
     indirect=True,
 )
-def test_list_nodes(client):
-    """
-    Verify that the list_nodes method can be poked successfully.
+class TestClientMethods:
+    def test_list_nodes(self, client: Client):
+        """
+        Verify that the list_nodes method can be poked successfully.
 
-    Args:
-        client: A Client instance.
-    """
-    nodes = client.list_nodes()
-    print(nodes)
+        Args:
+            client: A Client instance.
+        """
+        client.list_nodes()
+
+    def test_queue_node(self, client: Client):
+        """
+        Verify that an add node can be queued successfully.
+
+        Args:
+            client: A Client instance.
+        """
+        node_name = "demo.Add"
+        input = {
+            "lhs": 1,
+            "rhs": 2,
+        }  # Inputs are left hand side and right hand side of equation
+
+        job_id = client.queue_node(node=node_name, input=input)
+
+        # Add the job_id as an attribute of the test class so that it can be used in other tests
+        TestClientMethods.job_id = job_id
+
+    def test_job_status(self, client: Client):
+        """
+        Verify that the job_status method can be poked successfully.
+
+        Args:
+            client: A Client instance.
+        """
+        job_id = TestClientMethods.job_id
+        client.job_status(job_id)
+
+    def test_view_token(self, client: Client):
+        """
+        Verify that the view_token method can be poked successfully.
+
+        Args:
+            client: A Client instance.
+        """
+        client.view_tokens()
