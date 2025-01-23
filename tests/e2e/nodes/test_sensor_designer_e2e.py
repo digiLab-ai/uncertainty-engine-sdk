57c44bdf38a1c2b31191633e0d6fbad7bf4eab7d
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-01-23 12:25:14 +0000

    Add some addional e2e tests for the different input combinations of the BuildSensorDesigner class.

--- tests/e2e/nodes/test_sensor_designer_e2e.py
+++ tests/e2e/nodes/test_sensor_designer_e2e.py
@@ -137,3 +137,173 @@ class TestFullSet:
 
         assert status == "SUCCESS"
         assert "output" in response
+
+
+# NOTE: For these tests to run successfully, the following environment variables must be set:
+# UE_USER_EMAIL: A user email that has been registered with the Uncertainty Engine service.
+# UE_DEPLOYMENT_URL: The deployment URL for the Uncertainty Engine service.
+@pytest.mark.parametrize(
+    "test_user_email, deployment_url",
+    [(os.environ["UE_USER_EMAIL"], os.environ["UE_DEPLOYMENT_URL"])],
+    indirect=True,
+)
+class TestBuildNoQoI:
+    """
+    Verify successful execution of the BuildSensorDesigner node when no QoI data is provided.
+    """
+
+    def test_queue_build(self, client: Client):
+        """
+        Args:
+            client: A Client instance.
+        """
+        # Define the required data
+        sensor_data = {
+            "sensor_0_0": [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10],
+            "sensor_0_1": [1.11, 1.12, 1.13, 1.14, 1.15, 1.16, 1.17, 1.18, 1.19, 11.10],
+            "sensor_0_2": [2.21, 2.22, 2.23, 2.24, 2.25, 2.26, 2.27, 2.28, 2.29, 22.10],
+        }
+        sigma = 0.01
+
+        node = BuildSensorDesigner(
+            sensor_data=sensor_data,
+            sigma=sigma,
+        )
+
+        job_id = client.queue_node(node)
+
+        # Add the job_id as an attribute of the test class so that it can be used in other tests
+        TestBuildNoQoI.job_id_build = job_id
+
+    def test_result_build(self, client: Client):
+        """
+        Args:
+            client: A Client instance.
+        """
+        job_id = TestBuildNoQoI.job_id_build
+        response = client.job_status(job_id)
+
+        status = "STARTED"
+        while status not in ["SUCCESS", "FAILURE"]:
+            response = client.job_status(job_id)
+            status = response["status"]
+            time.sleep(5)
+
+        assert status == "SUCCESS"
+        assert "output" in response
+
+
+# NOTE: For these tests to run successfully, the following environment variables must be set:
+# UE_USER_EMAIL: A user email that has been registered with the Uncertainty Engine service.
+# UE_DEPLOYMENT_URL: The deployment URL for the Uncertainty Engine service.
+@pytest.mark.parametrize(
+    "test_user_email, deployment_url",
+    [(os.environ["UE_USER_EMAIL"], os.environ["UE_DEPLOYMENT_URL"])],
+    indirect=True,
+)
+class TestBuildNoSigma:
+    """
+    Verify successful execution of the BuildSensorDesigner node when no sigma is provided.
+    """
+
+    def test_queue_build(self, client: Client):
+        """
+        Args:
+            client: A Client instance.
+        """
+        # Define the required data
+        sensor_data = {
+            "sensor_0_0": [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10],
+            "sensor_0_1": [1.11, 1.12, 1.13, 1.14, 1.15, 1.16, 1.17, 1.18, 1.19, 11.10],
+            "sensor_0_2": [2.21, 2.22, 2.23, 2.24, 2.25, 2.26, 2.27, 2.28, 2.29, 22.10],
+        }
+        qoi_data = {
+            "qoi_1": [1.23, 1.32, 2.31, 2.13, 3.21, 3.12, 1.11, 2.22, 3.33, 32.23],
+            "qoi_2": [1.11, 2.22, 3.33, 4.44, 5.55, 6.66, 7.77, 8.88, 9.99, 1010.10],
+        }
+
+        node = BuildSensorDesigner(
+            sensor_data=sensor_data,
+            quantities_of_interest_data=qoi_data,
+        )
+
+        job_id = client.queue_node(node)
+
+        # Add the job_id as an attribute of the test class so that it can be used in other tests
+        TestBuildNoSigma.job_id_build = job_id
+
+    def test_result_build(self, client: Client):
+        """
+        Args:
+            client: A Client instance.
+        """
+        job_id = TestBuildNoSigma.job_id_build
+        response = client.job_status(job_id)
+
+        status = "STARTED"
+        while status not in ["SUCCESS", "FAILURE"]:
+            response = client.job_status(job_id)
+            status = response["status"]
+            time.sleep(5)
+
+        assert status == "SUCCESS"
+        assert "output" in response
+
+
+# NOTE: For these tests to run successfully, the following environment variables must be set:
+# UE_USER_EMAIL: A user email that has been registered with the Uncertainty Engine service.
+# UE_DEPLOYMENT_URL: The deployment URL for the Uncertainty Engine service.
+@pytest.mark.parametrize(
+    "test_user_email, deployment_url",
+    [(os.environ["UE_USER_EMAIL"], os.environ["UE_DEPLOYMENT_URL"])],
+    indirect=True,
+)
+class TestBuildListSigma:
+    """
+    Verify successful execution of the BuildSensorDesigner node when a list of sigma values is provided.
+    """
+
+    def test_queue_build(self, client: Client):
+        """
+        Args:
+            client: A Client instance.
+        """
+        # Define the required data
+        sensor_data = {
+            "sensor_0_0": [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10],
+            "sensor_0_1": [1.11, 1.12, 1.13, 1.14, 1.15, 1.16, 1.17, 1.18, 1.19, 11.10],
+            "sensor_0_2": [2.21, 2.22, 2.23, 2.24, 2.25, 2.26, 2.27, 2.28, 2.29, 22.10],
+        }
+        qoi_data = {
+            "qoi_1": [1.23, 1.32, 2.31, 2.13, 3.21, 3.12, 1.11, 2.22, 3.33, 32.23],
+            "qoi_2": [1.11, 2.22, 3.33, 4.44, 5.55, 6.66, 7.77, 8.88, 9.99, 1010.10],
+        }
+        sigma = [0.01, 0.02, 0.03]
+
+        node = BuildSensorDesigner(
+            sensor_data=sensor_data,
+            quantities_of_interest_data=qoi_data,
+            sigma=sigma,
+        )
+
+        job_id = client.queue_node(node)
+
+        # Add the job_id as an attribute of the test class so that it can be used in other tests
+        TestBuildListSigma.job_id_build = job_id
+
+    def test_result_build(self, client: Client):
+        """
+        Args:
+            client: A Client instance.
+        """
+        job_id = TestBuildListSigma.job_id_build
+        response = client.job_status(job_id)
+
+        status = "STARTED"
+        while status not in ["SUCCESS", "FAILURE"]:
+            response = client.job_status(job_id)
+            status = response["status"]
+            time.sleep(5)
+
+        assert status == "SUCCESS"
+        assert "output" in response
