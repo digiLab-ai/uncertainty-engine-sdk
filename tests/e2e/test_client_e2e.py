269f71d7da1698363fb43ea6f19149f4b1a69ef6
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-01-23 13:27:59 +0000

    Make the client e2e tests use the new e2e client fixture.

--- tests/e2e/test_client_e2e.py
+++ tests/e2e/test_client_e2e.py
@@ -1,35 +1,24 @@
-import os
 import time
-
-import pytest
 
 from uncertainty_engine.client import Client
 
 
-# NOTE: For these tests to run successfully, the following environment variables must be set:
-# UE_USER_EMAIL: A user email that has been registered with the Uncertainty Engine service.
-# UE_DEPLOYMENT_URL: The deployment URL for the Uncertainty Engine service.
-@pytest.mark.parametrize(
-    "test_user_email, deployment_url",
-    [(os.environ["UE_USER_EMAIL"], os.environ["UE_DEPLOYMENT_URL"])],
-    indirect=True,
-)
 class TestClientMethods:
-    def test_list_nodes(self, client: Client):
+    def test_list_nodes(self, e2e_client: Client):
         """
         Verify that the list_nodes method can be poked successfully.
 
         Args:
-            client: A Client instance.
+            e2e_client: A Client instance.
         """
-        client.list_nodes()
+        e2e_client.list_nodes()
 
-    def test_queue_node(self, client: Client):
+    def test_queue_node(self, e2e_client: Client):
         """
         Verify that an add node can be queued successfully.
 
         Args:
-            client: A Client instance.
+            e2e_client: A Client instance.
         """
         node_name = "demo.Add"
         inputs = {
@@ -37,34 +26,34 @@ class TestClientMethods:
             "rhs": 2,
         }  # Inputs are left hand side and right hand side of equation
 
-        job_id = client.queue_node(node=node_name, input=inputs)
+        job_id = e2e_client.queue_node(node=node_name, input=inputs)
 
         # Add the job_id as an attribute of the test class so that it can be used in other tests
         TestClientMethods.job_id = job_id
 
-    def test_job_status(self, client: Client):
+    def test_job_status(self, e2e_client: Client):
         """
         Verify that the job_status method can be poked successfully.
 
         Args:
-            client: A Client instance.
+            e2e_client: A Client instance.
         """
         job_id = TestClientMethods.job_id
-        response = client.job_status(job_id)
+        response = e2e_client.job_status(job_id)
 
         status = "STARTED"
         while status == "STARTED":
-            response = client.job_status(job_id)
+            response = e2e_client.job_status(job_id)
             status = response["status"]
             time.sleep(5)
 
         assert response["status"] == "SUCCESS"
 
-    def test_view_token(self, client: Client):
+    def test_view_token(self, e2e_client: Client):
         """
         Verify that the view_token method can be poked successfully.
 
         Args:
-            client: A Client instance.
+            e2e_client: A Client instance.
         """
-        client.view_tokens()
+        e2e_client.view_tokens()
