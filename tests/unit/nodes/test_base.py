41a77103920d1f8ca83e3a90b34f3ee3ab24b481
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-01-29 12:00:40 +0000

    Update tests to reflect change from name to node_name.

--- tests/unit/nodes/test_base.py
+++ tests/unit/nodes/test_base.py
@@ -8,7 +8,7 @@ def test_node():
     Verify result for arbitrary test node.
     """
     node = Node("test_node", a=1, b=2)
-    assert node.name == "test_node"
+    assert node.node_name == "test_node"
     assert node.a == 1
     assert node.b == 2
     assert node() == ("test_node", {"a": 1, "b": 2})
@@ -19,7 +19,7 @@ def test_node_no_inputs():
     Verify result for test node with no inputs.
     """
     node = Node("test_node")
-    assert node.name == "test_node"
+    assert node.node_name == "test_node"
     assert node() == ("test_node", {})
 
 
