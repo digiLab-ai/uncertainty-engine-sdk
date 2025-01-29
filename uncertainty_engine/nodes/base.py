bfef8b3087632002e9e60d127fb1708faef2e78f
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-01-29 10:58:18 +0000

    Use node_name rather than name as Node attribute name. More specific. Doesn't polute with generic name.

--- uncertainty_engine/nodes/base.py
+++ uncertainty_engine/nodes/base.py
@@ -12,7 +12,7 @@ class Node:
     """
 
     def __init__(self, node_name: str, **kwargs):
-        self.name = node_name
+        self.node_name = node_name
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -25,4 +25,4 @@ class Node:
             A tuple containing the name of the node and the input parameters.
         """
         input = {key: getattr(self, key) for key in self.__dict__ if key != "name"}
-        return self.name, input
+        return self.node_name, input
