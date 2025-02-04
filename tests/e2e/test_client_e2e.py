0aa5df6385b48f3cb0000232f267b0a166fa9b05
Jamie Donald-McCann <61426038+JDonaldM@users.noreply.github.com>
2025-02-04 09:35:15 +0000

    Add a simple e2e test that verifies an error message is returned by the API when there is a failure on the backend. (#9)

--- tests/e2e/test_client_e2e.py
+++ tests/e2e/test_client_e2e.py
@@ -57,3 +57,29 @@ class TestClientMethods:
             e2e_client: A Client instance.
         """
         e2e_client.view_tokens()
+
+
+def test_failure_bad_client(e2e_client: Client):
+    """
+    Verify an error is returned when an unknown client is used.
+    """
+
+    client = e2e_client
+    client.email = "not_a_valid_email@mail.com"
+
+    node_name = "demo.Add"
+    inputs = {
+        "lhs": 1,
+        "rhs": 2,
+    }  # Inputs are left hand side and right hand side of equation
+
+    job_id = client.queue_node(node=node_name, input=inputs)
+
+    status = "PENDING"
+    while status == "PENDING":
+        response = e2e_client.job_status(job_id)
+        status = response["status"]
+        time.sleep(5)
+
+    assert response["status"] == "FAILURE"
+    assert "error" in response
