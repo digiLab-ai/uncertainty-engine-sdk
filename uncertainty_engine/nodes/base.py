6757e61a30618f6d2ee39a36d4155b3a471e7619
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-02-11 13:32:53 +0000

    Updates to the base Node class. Add label variable to the init. This can then be used if the node is added to a graph. Add make_handle method that otupts a Handle object. Again can be used when constructing a graph with the node.

--- uncertainty_engine/nodes/base.py
+++ uncertainty_engine/nodes/base.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from typeguard import typechecked
+from uncertainty_engine_types import Handle
 
 
 @typechecked
@@ -8,11 +11,13 @@ class Node:
 
     Args:
         node_name: The name of the node.
+        label: A human-readable label for the node. Defaults to None.
         **kwargs: Arbitrary keyword arguments representing the input parameters of the node.
     """
 
-    def __init__(self, node_name: str, **kwargs):
+    def __init__(self, node_name: str, label: Optional[str] = None, **kwargs):
         self.node_name = node_name
+        self.label = label
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -24,5 +29,24 @@ class Node:
         Returns:
             A tuple containing the name of the node and the input parameters.
         """
-        input = {key: getattr(self, key) for key in self.__dict__ if key != "node_name"}
+        input = {
+            key: getattr(self, key)
+            for key in self.__dict__
+            if key not in ["node_name", "label"]
+        }
         return self.node_name, input
+
+    def make_handle(self, output_name: str) -> Handle:
+        """
+        Make a handle for the output of the node.
+
+        Args:
+            output_name: The name of the output.
+
+        Returns:
+            A string handle for the output.
+        """
+        if self.label is None:
+            raise ValueError("Nodes must have a label to make a handle.")
+
+        return Handle(f"{self.label}.{output_name}")
