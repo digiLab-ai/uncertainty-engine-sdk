43597e78e585ec4ede6d4b11437a6da5b50ffbcd
jacantwell <jasper66018@gmail.com>
2025-05-06 11:42:15 +0100

    Adding provider class for auth (currently just sets the account id)

---
+++ uncertainty_engine/auth_provider.py
@@ -0,0 +1,14 @@
+class AuthProvider:
+    """Authentication state management"""
+
+    def __init__(self):
+        self.account_id = None
+
+    def authenticate(self, account_id: str) -> None:
+        """Set authentication credentials"""
+        self.account_id = account_id
+
+    @property
+    def is_authenticated(self) -> bool:
+        """Check if authentication has been performed"""
+        return self.account_id is not None
