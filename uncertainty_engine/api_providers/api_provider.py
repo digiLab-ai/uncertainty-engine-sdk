298c1e7cd1948a08f331035e240965a427de9d1b
jacantwell <jasper66018@gmail.com>
2025-05-12 11:23:15 +0100

    renaming baseurl to deployment

--- uncertainty_engine/api_providers/api_provider.py
+++ uncertainty_engine/api_providers/api_provider.py
@@ -4,6 +4,6 @@ from uncertainty_engine.auth_service import AuthService
 class ApiProviderBase:
     """Base class for all API clients with automatic token refresh"""
 
-    def __init__(self, base_url: str, auth_service: AuthService):
-        self.base_url = base_url
+    def __init__(self, deployment: str, auth_service: AuthService):
+        self.deployment = deployment
         self.auth_service = auth_service
