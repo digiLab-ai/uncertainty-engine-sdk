d33619b44f0f8f1492d7daac7ac88f1a53b55bc4
natalietf <natalie.tripp.foulds@gmail.com>
2025-05-22 09:42:54 +0100

    examples in node doctring

--- uncertainty_engine/nodes/base.py
+++ uncertainty_engine/nodes/base.py
@@ -13,6 +13,17 @@ class Node:
         node_name: The name of the node.
         label: A human-readable label for the node. Defaults to None.
         **kwargs: Arbitrary keyword arguments representing the input parameters of the node.
+
+    Example:
+    ```python
+      add_node = Node(
+          node_name="Add",
+          label="My Add Node",
+          lhs=1,
+          rhs=2
+      )
+    ```
+
     """
 
     def __init__(self, node_name: str, label: Optional[str] = None, **kwargs):
