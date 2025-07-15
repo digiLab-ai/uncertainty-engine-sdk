56ce615852df89f0e5a3b180738130bc26f16db1
natalietf <natalie.tripp.foulds@gmail.com>
2025-07-15 11:39:57 +0100

    tests

--- tests/unit/api_providers/conftest.py
+++ tests/unit/api_providers/conftest.py
@@ -3,6 +3,7 @@ from unittest.mock import MagicMock, patch
 import pytest
 from uncertainty_engine_resource_client.api.project_records_api import ProjectRecordsApi
 from uncertainty_engine_resource_client.api.resources_api import ResourcesApi
+from uncertainty_engine_resource_client.api.account_records_api import AccountRecordsApi
 from uncertainty_engine_resource_client.api_client import ApiClient
 from uncertainty_engine_resource_client.configuration import Configuration
 
@@ -25,6 +26,13 @@ def mock_api_client(mock_api_configuration: Configuration):
 
 
 @pytest.fixture
+def mock_account_records_api(mock_api_client: ApiClient):
+    accounts_api = MagicMock()
+    accounts_api.api_client = mock_api_client
+    return accounts_api
+
+
+@pytest.fixture
 def mock_project_records_api(mock_api_client: ApiClient):
     project_records_api = MagicMock()
     project_records_api.api_client = mock_api_client
@@ -33,7 +41,7 @@ def mock_project_records_api(mock_api_client: ApiClient):
 
 @pytest.fixture
 def mock_resources_api(mock_api_client: ApiClient):
-    resources_api = MagicMock()
+    resources_api = MagicMock(spec=AccountRecordsApi)
     resources_api.api_client = mock_api_client
     return resources_api
 
@@ -41,13 +49,18 @@ def mock_resources_api(mock_api_client: ApiClient):
 @pytest.fixture
 def mocked_api_clients(
     mock_api_client: ApiClient,
+    mock_account_records_api: AccountRecordsApi,
     mock_project_records_api: ProjectRecordsApi,
     mock_resources_api: ResourcesApi,
 ):
-    """Sets up mocked API clients for ResourceProvider initialization."""
+    """Sets up mocked API clients for Provider initialization."""
     api_client_patch = patch(
         "uncertainty_engine_resource_client.api_client.ApiClient",
         return_value=mock_api_client,
+    )
+    account_records_api_patch = patch(
+        "uncertainty_engine_resource_client.api.account_records_api.AccountRecordsApi",
+        return_value=mock_account_records_api,
     )
     project_records_api_patch = patch(
         "uncertainty_engine_resource_client.api.project_records_api.ProjectRecordsApi",
@@ -58,7 +71,9 @@ def mocked_api_clients(
         return_value=mock_resources_api,
     )
 
-    with api_client_patch, project_records_api_patch, resources_api_patch:
+    with (
+        api_client_patch
+    ), project_records_api_patch, resources_api_patch, account_records_api_patch:
         yield
 
 
