3a144f92bdb292611b12e7218b9bbc9a4479390b
natalietf <natalie.tripp.foulds@gmail.com>
2025-09-03 17:26:56 +0100

    deprectaion warnings in Workflow class

--- uncertainty_engine/nodes/workflow.py
+++ uncertainty_engine/nodes/workflow.py
@@ -1,5 +1,7 @@
-from typeguard import typechecked
+import warnings
 from typing import Any
+
+from typeguard import typechecked
 
 from uncertainty_engine.nodes.base import Node
 
@@ -11,10 +13,15 @@ class Workflow(Node):
 
     Args:
         graph: The graph of nodes to execute.
-        inputs: The inputs to the workflow.
         requested_output: The requested output from the workflow.
-        external_input_id: String identifier that refers to external inputs to the graph.
-            Default is "_".
+        external_input_id: String identifier that refers to external inputs to the
+            graph. Default is "_".
+        inputs: The inputs to the workflow. Defaults to None.
+        input: **DEPRECATED** The inputs to the workflow. Use `inputs` instead.
+            Will be removed in a future version.
+
+    Raises:
+        ValueError: if both `inputs` and `input` are `None`, or if both are provided.
 
     Example:
         >>> workflow = Workflow(
@@ -33,14 +40,35 @@ class Workflow(Node):
     def __init__(
         self,
         graph: dict[str, Any],
-        inputs: dict[str, Any],
         requested_output: dict[str, Any],
         external_input_id: str = "_",
+        # TODO: Make this required once `input` parameter is removed
+        inputs: dict[str, Any] | None = None,
+        # Deprecated: Use `inputs` instead. Will be removed in next release
+        input: dict[str, Any] | None = None,
     ):
+        if input is not None and inputs is not None:
+            raise ValueError(
+                "Cannot specify both 'input' and 'inputs'. Use 'inputs' only."
+            )
+
+        if input is not None:
+            warnings.warn(
+                "The 'input' parameter is deprecated and will be removed in the next "
+                "release. Use 'inputs' instead.",
+                DeprecationWarning,
+                stacklevel=2,
+            )
+            final_inputs = input
+        elif inputs is not None:
+            final_inputs = inputs
+        else:
+            raise ValueError("'inputs' must be provided.")
+
         super().__init__(
             node_name=self.node_name,
             external_input_id=external_input_id,
             graph=graph,
-            inputs=inputs,
+            inputs=final_inputs,
             requested_output=requested_output,
         )
