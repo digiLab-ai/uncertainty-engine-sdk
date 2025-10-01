7da6c9f5672639d64ec5a8fe0dd2f3e233d2c200
eirinikouvara <eirinikouvara@hotmail.com>
2025-10-01 10:42:39 +0100

    PB-1108: add class for LoadDataset (#114)

---
+++ uncertainty_engine/nodes/resource_management.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+from typeguard import typechecked
+from uncertainty_engine_types import ResourceID
+
+from uncertainty_engine.nodes.base import Node
+
+
+@typechecked
+class LoadDataset(Node):
+    """
+    Load a dataset from the Uncertainty Engine resource management
+    system.
+
+    Args:
+       project_id: The ID of the project containing the dataset.
+       file_id: The ID of the dataset file to load.
+       label: A human-readable label for the node. Defaults to None.
+    """
+
+    node_name: str = "LoadDataset"
+
+    def __init__(
+        self,
+        project_id: str,
+        file_id: ResourceID,
+        label: Optional[str] = None,
+    ):
+        super().__init__(
+            node_name=self.node_name,
+            label=label,
+            project_id=project_id,
+            file_id=file_id,
+        )
+        self.project_id = project_id
+        self.file_id = file_id
+        self.label = label
