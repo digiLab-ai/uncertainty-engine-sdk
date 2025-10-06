a7b773b75d3cd07e689e72d8e3e4d27e4d09efd6
Natalie Tripp Foulds <83218927+natalietf@users.noreply.github.com>
2025-10-06 14:04:19 +0100

    `PB-1110:` SDK class for save node (#116)
    * add save class
    
    * add test

--- uncertainty_engine/nodes/resource_management.py
+++ uncertainty_engine/nodes/resource_management.py
@@ -1,9 +1,10 @@
 from typing import Optional
 
 from typeguard import typechecked
-from uncertainty_engine_types import ResourceID
+from uncertainty_engine_types import ResourceID, S3Storage
 
 from uncertainty_engine.nodes.base import Node
+from uncertainty_engine.utils import HandleUnion
 
 
 @typechecked
@@ -34,3 +35,56 @@ class LoadDataset(Node):
         )
         self.project_id = project_id
         self.label = label
+
+
+@typechecked
+class Save(Node):
+    """
+    Save a resource in the Uncertainty Engine resource management
+    system.
+
+    Note that this is for saving resources that are output by a node.
+    If you wish to upload a file to use in your workflow then you should
+    use the resource provider (use `client.resources.upload()` to upload
+    a local resource to the Uncertainty Engine).
+    """
+
+    data: HandleUnion[S3Storage]
+    """A reference to the node output data to be saved."""
+
+    file_id: str
+    """
+    The human-readable name for the saved resource. If a resource of the
+    same type and name already exists, the save node will create a new
+    version of that existing resource.
+    """
+
+    project_id: str
+    """The ID of the project to save to."""
+
+    label: str | None
+    """
+    A human-readable label for the node. This should be unique to all
+    other node labels in a workflow.
+    """
+
+    node_name: str = "Save"
+    """The node ID."""
+
+    def __init__(
+        self,
+        data: HandleUnion[S3Storage],
+        file_name: str,
+        project_id: str,
+        label: str | None = None,
+    ):
+        super().__init__(
+            node_name=self.node_name,
+            label=label,
+            data=data,
+            # NOTE: The underlying node input ID for the file name is
+            # `file_id`, but we refer to it as `file_name` in this class
+            # for clarity and ease of use.
+            file_id=file_name,
+            project_id=project_id,
+        )
