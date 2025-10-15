74cb798e84917408f185484dd1d8f2c9e80a0168
Henry80074 <89064748+Henry80074@users.noreply.github.com>
2025-10-15 11:30:11 +0100

    Pb 1131 predict mpdel= sdk (#127)
    * add sdk class and test
    
    * add train model class and test
    
    * add predict
    
    * add predict model sdk code
    
    * format
    
    * only predict
    
    * fix up conflicts
    
    * fix conflicts
    
    * add file
    
    * update test
    
    * conflicts
    
    * add types
    
    ---------
    
    Co-authored-by: Henry Booth <henry.booth@digilab.ai>

--- uncertainty_engine/nodes/machine_learning.py
+++ uncertainty_engine/nodes/machine_learning.py
@@ -55,3 +55,47 @@ class TrainModel(Node):
             outputs=outputs,
             project_id=project_id,
         )
+
+
+@typechecked
+class PredictModel(Node):
+    """
+    Run predictions using a trained machine-learning model in the Uncertainty Engine.
+
+    Args:
+        model: A reference to the trained machine-learning model to use for prediction.
+        inputs: A reference to the input dataset for making predictions.
+        label: A human-readable label for the node. This should be unique to all
+               other node labels in a workflow. Defaults to "Predict Model".
+        project_id: The ID of the project to associate with this node.
+    """
+
+    node_name: str = "PredictModel"
+    """The node ID."""
+
+    label: str = "Predict Model"
+    """A human-readable label for the node."""
+
+    model: HandleUnion[S3Storage]
+    """A reference to the trained machine-learning model to use for prediction."""
+
+    dataset: HandleUnion[S3Storage]
+    """A reference to the input dataset for making predictions."""
+
+    project_id: Optional[str] = None
+    """The ID of the project to associate with this node."""
+
+    def __init__(
+        self,
+        model: HandleUnion[S3Storage],
+        dataset: HandleUnion[S3Storage],
+        label: Optional[str] = None,
+        project_id: Optional[str] = None,
+    ):
+        super().__init__(
+            node_name=self.node_name,
+            label=label or self.label,
+            model=model,
+            dataset=dataset,
+            project_id=project_id,
+        )
