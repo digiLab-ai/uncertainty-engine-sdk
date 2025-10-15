c5a0277fb1385be17179cd48d95067ab4c012407
Henry80074 <89064748+Henry80074@users.noreply.github.com>
2025-10-15 10:39:12 +0100

    Pb 1130 train mopdel sdk (#125)
    * add sdk class and test
    
    * add train model class and test
    
    * fix to use storage
    
    * froamt
    
    ---------
    
    Co-authored-by: Henry Booth <henry.booth@digilab.ai>

---
+++ tests/unit/nodes/test_machine_learning.py
@@ -0,0 +1,29 @@
+from uncertainty_engine_types import Handle, ModelConfig, S3Storage
+
+from uncertainty_engine.nodes.machine_learning import TrainModel
+
+
+def test_train_model_initialization():
+    """Test the initialization of the TrainModel node."""
+
+    # Example values, test with handles and direct objects
+    config = ModelConfig()
+    inputs = S3Storage(bucket="my-bucket", key="input.csv")
+    outputs = Handle(node_name="OutputNode", node_handle="outputs")
+    label = "Test Train Model"
+    project_id = "projectid-123"
+
+    node = TrainModel(
+        config=config,
+        inputs=inputs,
+        outputs=outputs,
+        label=label,
+        project_id=project_id,
+    )
+
+    assert node.node_name == "TrainModel"
+    assert node.config == config
+    assert node.inputs == inputs
+    assert node.outputs == outputs
+    assert node.label == label
+    assert node.project_id == project_id
