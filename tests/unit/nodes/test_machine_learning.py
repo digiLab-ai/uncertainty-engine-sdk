06d5068a64dcb72231da8cf8bbbe5628506c05e2
Henry Booth <henry.booth@digilab.ai>
2025-10-15 12:03:41 +0100

    Revert "Pb 1131 predict mpdel= sdk (#127)"
    This reverts commit 74cb798e84917408f185484dd1d8f2c9e80a0168.

--- tests/unit/nodes/test_machine_learning.py
+++ tests/unit/nodes/test_machine_learning.py
@@ -1,6 +1,6 @@
 from uncertainty_engine_types import Handle, ModelConfig, S3Storage
 
-from uncertainty_engine.nodes.machine_learning import PredictModel, TrainModel
+from uncertainty_engine.nodes.machine_learning import TrainModel
 
 
 def test_train_model_initialization():
@@ -25,28 +25,5 @@ def test_train_model_initialization():
     assert node.config == config
     assert node.inputs == inputs
     assert node.outputs == outputs
-    assert node.label == label
-    assert node.project_id == project_id
-
-
-def test_predict_model_initialization():
-    """Test the initialization of the PredictModel node."""
-
-    # Example values, test with handles and direct objects
-    model = Handle(node_name="TrainModelNode", node_handle="model")
-    dataset = S3Storage(bucket="my-bucket", key="predict_input.csv")
-    label = "Test Predict Model"
-    project_id = "projectid-456"
-
-    node = PredictModel(
-        model=model,
-        dataset=dataset,
-        label=label,
-        project_id=project_id,
-    )
-
-    assert node.node_name == "PredictModel"
-    assert node.model == model
-    assert node.dataset == dataset
     assert node.label == label
     assert node.project_id == project_id
