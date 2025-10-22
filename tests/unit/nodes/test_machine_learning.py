773082b22108d610bd72489b86d3fdf87cc2b800
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-10-22 10:58:47 +0100

    Sort test functions into alphabetical order

--- tests/unit/nodes/test_machine_learning.py
+++ tests/unit/nodes/test_machine_learning.py
@@ -36,6 +36,29 @@ def test_model_config_initialization():
     assert node.label == "Model Config"
 
 
+def test_predict_model_initialization():
+    """Test the initialization of the PredictModel node."""
+
+    # Example values, test with handles and direct objects
+    model = Handle(node_name="TrainModelNode", node_handle="model")
+    dataset = S3Storage(bucket="my-bucket", key="predict_input.csv")
+    label = "Test Predict Model"
+    project_id = "projectid-456"
+
+    node = PredictModel(
+        model=model,
+        dataset=dataset,
+        label=label,
+        project_id=project_id,
+    )
+
+    assert node.node_name == "PredictModel"
+    assert node.model == model
+    assert node.dataset == dataset
+    assert node.label == label
+    assert node.project_id == project_id
+
+
 def test_train_model_initialization():
     """Test the initialization of the TrainModel node."""
 
@@ -58,28 +81,5 @@ def test_train_model_initialization():
     assert node.config == config
     assert node.inputs == inputs
     assert node.outputs == outputs
-    assert node.label == label
-    assert node.project_id == project_id
-
-
-def test_predict_model_initialization():
-    """Test the initialization of the PredictModel node."""
-
-    # Example values, test with handles and direct objects
-    model = Handle(node_name="TrainModelNode", node_handle="model")
-    dataset = S3Storage(bucket="my-bucket", key="predict_input.csv")
-    label = "Test Predict Model"
-    project_id = "projectid-456"
-
-    node = PredictModel(
-        model=model,
-        dataset=dataset,
-        label=label,
-        project_id=project_id,
-    )
-
-    assert node.node_name == "PredictModel"
-    assert node.model == model
-    assert node.dataset == dataset
     assert node.label == label
     assert node.project_id == project_id
