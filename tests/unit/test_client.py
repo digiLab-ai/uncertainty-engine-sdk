a98c02bcd1858551c2ed646a35302964d3445646
Nathan Creaser <nathancreaser88@gmail.com>
2025-10-23 11:49:03 +0100

    update existing test_queue_node_node_input  unit tests to assert empty tool metadata

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -156,7 +156,7 @@ class TestClientMethods:
                 "/nodes/queue",
                 expect_body={
                     "node_id": mock_job.node_id,
-                    "inputs": {"key": "value"},
+                    "inputs": {"key": "value", "tool_metadata": {}},
                 },
                 response=mock_job.job_id,
             )
