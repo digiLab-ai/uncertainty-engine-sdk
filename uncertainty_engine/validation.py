54fcae0a15375db74eff7fa1899583c020defb37
Natalie Tripp Foulds <83218927+natalietf@users.noreply.github.com>
2025-11-13 11:46:17 +0000

    `PB-1310:` Refactor `make_handle` validation logic to use a validate function (#165)
    * add validate_outputs_exist
    
    * have output validation take a list and add tests
    
    * docstring
    
    * update warning message
    
    * imporove commenting
    
    * fix punctuation

--- uncertainty_engine/validation.py
+++ uncertainty_engine/validation.py
@@ -51,3 +51,26 @@ def validate_inputs_exist(node_info: NodeInfo, node_inputs: dict[str, Any]) -> N
 
     if invalid_input_names:
         raise NodeValidationError(f"Invalid input names: {invalid_input_names}")
+
+
+def validate_outputs_exist(node_info: NodeInfo, node_outputs: list[str]) -> None:
+    """
+    Validates that all output names referenced in the `node_outputs` are
+    existing output names (as defined by the `node_info`).
+
+    Args:
+        node_info: A `NodeInfo` object used to validate the node outputs
+            against.
+        node_outputs: A list containing the node output names.
+
+    Raises:
+        `NodeValidationError`: If there are 1 or more invalid output
+            names. A list of the invalid output names is returned as
+            part of the error message.
+    """
+    invalid_output_names = [
+        name for name in node_outputs if (name not in node_info.outputs)
+    ]
+
+    if invalid_output_names:
+        raise NodeValidationError(f"Invalid output names: {invalid_output_names}")
