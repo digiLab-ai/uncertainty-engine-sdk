eb3c58c4f50a93c5bb8b7e6e0a7ad58dec7818ba
Natalie Tripp Foulds <83218927+natalietf@users.noreply.github.com>
2025-11-18 09:59:01 +0000

    `PB-1310` Update validate outputs to take one or multiple names (#167)
    * add typechecked decorator
    
    * update validate outputs to take a str or list of str
    
    * update docstring
    
    * correct typing

--- tests/unit/test_validation.py
+++ tests/unit/test_validation.py
@@ -173,7 +173,7 @@ def test_validate_inputs_exist_errors(
             },
         ),
         (
-            ["a"],
+            "a",
             {
                 "a": NodeOutputInfo(type="", label="", description=""),
             },
@@ -189,7 +189,7 @@ def test_validate_inputs_exist_errors(
 )
 def test_validate_outputs_exist(
     default_node_info: NodeInfo,
-    node_outputs: list[str],
+    node_outputs: str | list[str],
     node_info_outputs: dict[str, NodeOutputInfo],
 ):
     """
@@ -212,7 +212,7 @@ def test_validate_outputs_exist(
             "Invalid output names: ['x']",
         ),
         (
-            ["y"],
+            "y",
             {
                 "a": NodeOutputInfo(type="", label="", description=""),
             },
@@ -230,7 +230,7 @@ def test_validate_outputs_exist(
 )
 def test_validate_outputs_exist_errors(
     default_node_info: NodeInfo,
-    node_outputs: list[str],
+    node_outputs: str | list[str],
     node_info_outputs: dict[str, NodeOutputInfo],
     expected_error: str,
 ):
--- uncertainty_engine/validation.py
+++ uncertainty_engine/validation.py
@@ -1,10 +1,12 @@
 from typing import Any
 
+from typeguard import typechecked
 from uncertainty_engine_types import NodeInfo
 
 from uncertainty_engine.exceptions import NodeValidationError
 
 
+@typechecked
 def validate_required_inputs(node_info: NodeInfo, node_inputs: dict[str, Any]) -> None:
     """
     Validates that all required inputs (as listed in the `node_info`)
@@ -30,6 +32,7 @@ def validate_required_inputs(node_info: NodeInfo, node_inputs: dict[str, Any]) -
         raise NodeValidationError(f"Missing required inputs: {missing_inputs}")
 
 
+@typechecked
 def validate_inputs_exist(node_info: NodeInfo, node_inputs: dict[str, Any]) -> None:
     """
     Validates that all input names referenced in the `node_inputs` are
@@ -53,7 +56,8 @@ def validate_inputs_exist(node_info: NodeInfo, node_inputs: dict[str, Any]) -> N
         raise NodeValidationError(f"Invalid input names: {invalid_input_names}")
 
 
-def validate_outputs_exist(node_info: NodeInfo, node_outputs: list[str]) -> None:
+@typechecked
+def validate_outputs_exist(node_info: NodeInfo, node_outputs: str | list[str]) -> None:
     """
     Validates that all output names referenced in the `node_outputs` are
     existing output names (as defined by the `node_info`).
@@ -61,13 +65,17 @@ def validate_outputs_exist(node_info: NodeInfo, node_outputs: list[str]) -> None
     Args:
         node_info: A `NodeInfo` object used to validate the node outputs
             against.
-        node_outputs: A list containing the node output names.
+        node_outputs: A single or a list of multiple node output names
+            to validate.
 
     Raises:
         `NodeValidationError`: If there are 1 or more invalid output
             names. A list of the invalid output names is returned as
             part of the error message.
     """
+    if isinstance(node_outputs, str):
+        node_outputs = [node_outputs]
+
     invalid_output_names = [
         name for name in node_outputs if (name not in node_info.outputs)
     ]
