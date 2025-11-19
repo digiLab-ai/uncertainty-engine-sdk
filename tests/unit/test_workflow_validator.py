5ec2e6c865e98cf26f93b18f30db0c8059119e06
natalietf <natalie.tripp.foulds@gmail.com>
2025-11-19 15:43:24 +0000

    all tests

--- tests/unit/test_workflow_validator.py
+++ tests/unit/test_workflow_validator.py
@@ -4,7 +4,11 @@ from unittest.mock import patch
 from pytest import mark, raises
 from uncertainty_engine_types import NodeElement, NodeInfo
 
-from uncertainty_engine.exceptions import NodeErrorInfo, WorkflowValidationError
+from uncertainty_engine.exceptions import (
+    NodeErrorInfo,
+    NodeValidationError,
+    WorkflowValidationError,
+)
 from uncertainty_engine.workflow_validator import WorkflowValidator
 
 
@@ -92,10 +96,20 @@ def test_workflow_validator_init_value_error(
         WorkflowValidator(node_info_list, invalid_graph)
 
 
-def test_workflow_validator_validate_node_inputs_no_errors(
+@mark.parametrize(
+    "validation_errors",
+    [
+        ["missing required inputs", "inputs do not exist"],
+        [None, "inputs do not exist"],
+        ["missing required inputs", None],
+        [None, None],
+    ],
+)
+def test_workflow_validator_validate_node_inputs(
     add_node_info: NodeInfo,
     node_info_list: list[NodeInfo],
     workflow_node_graph: dict[str, Any],
+    validation_errors: list[str | None],
 ):
     validator = WorkflowValidator(
         node_info_list=node_info_list,
@@ -111,12 +125,25 @@ def test_workflow_validator_validate_node_inputs_no_errors(
     ) as mock_validate_req, patch(
         "uncertainty_engine.workflow_validator.validate_inputs_exist"
     ) as mock_validate_exist:
+        mock_validate_req.side_effect = (
+            NodeValidationError(validation_errors[0]) if validation_errors[0] else None
+        )
+        mock_validate_exist.side_effect = (
+            NodeValidationError(validation_errors[1]) if validation_errors[1] else None
+        )
+
         validator._validate_node_inputs(test_node)
 
         mock_validate_req.assert_called_once_with(add_node_info, node_element.inputs)
         mock_validate_exist.assert_called_once_with(add_node_info, node_element.inputs)
 
-    assert validator.node_errors == []
+    expected_errors = [
+        NodeErrorInfo(node_id="Test Add", message=msg)
+        for msg in validation_errors
+        if msg
+    ]
+
+    assert validator.node_errors == expected_errors
 
 
 def test_workflow_validator_validate_node_inputs_node_type_error(
