63f6239a0ee11d85f0241882994e2b2dedc1578e
natalietf <natalie.tripp.foulds@gmail.com>
2025-11-19 10:39:06 +0000

    unneeded f string

--- uncertainty_engine/workflow_validator.py
+++ uncertainty_engine/workflow_validator.py
@@ -59,7 +59,7 @@ class WorkflowValidator:
 
         except ValidationError as e:
             raise WorkflowValidationError(
-                f"Invalid workflow graph:\n" + format_pydantic_error(e)
+                "Invalid workflow graph:\n" + format_pydantic_error(e)
             )
 
         self.inputs = inputs
