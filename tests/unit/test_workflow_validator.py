6f1546bb6baa8c684f60fefdcb2ff08f2a5e863a
Natalie Tripp Foulds <83218927+natalietf@users.noreply.github.com>
2025-11-19 16:01:15 +0000

    `PB-1310:` Add node input validation method to `WorkflowValidator` (#171)
    * add node input validation to WorkflowValidator
    
    * improve commenting
    
    * add no errors test
    
    * add no errors test
    
    * all tests
    
    * add test docstrings
    
    * formatting

--- tests/unit/test_workflow_validator.py
+++ tests/unit/test_workflow_validator.py
@@ -1,10 +1,15 @@
 from typing import Any
+from unittest.mock import patch
 
 from pytest import mark, raises
-from uncertainty_engine_types import NodeInfo
+from uncertainty_engine_types import NodeElement, NodeInfo
 
+from uncertainty_engine.exceptions import (
+    NodeErrorInfo,
+    NodeValidationError,
+    WorkflowValidationError,
+)
 from uncertainty_engine.workflow_validator import WorkflowValidator
-from uncertainty_engine.exceptions import WorkflowValidationError
 
 
 def test_workflow_validator_init(
@@ -89,3 +94,80 @@ def test_workflow_validator_init_value_error(
         match=expected_error,
     ):
         WorkflowValidator(node_info_list, invalid_graph)
+
+
+@mark.parametrize(
+    "validation_errors",
+    [
+        ["missing required inputs", "inputs do not exist"],
+        [None, "inputs do not exist"],
+        ["missing required inputs", None],
+        [None, None],
+    ],
+)
+def test_workflow_validator_validate_node_inputs(
+    add_node_info: NodeInfo,
+    node_info_list: list[NodeInfo],
+    workflow_node_graph: dict[str, Any],
+    validation_errors: list[str | None],
+):
+    """
+    Assert `_validate_node_inputs` stores input validation errors under
+    `self.node_errors` with associated node id and error message.
+    """
+    validator = WorkflowValidator(
+        node_info_list=node_info_list,
+        graph=workflow_node_graph,
+    )
+
+    node_id = "Test Add"
+    node_element = NodeElement(**workflow_node_graph["nodes"][node_id])
+    test_node = (node_id, node_element)
+
+    with patch(
+        "uncertainty_engine.workflow_validator.validate_required_inputs"
+    ) as mock_validate_req, patch(
+        "uncertainty_engine.workflow_validator.validate_inputs_exist"
+    ) as mock_validate_exist:
+        mock_validate_req.side_effect = (
+            NodeValidationError(validation_errors[0]) if validation_errors[0] else None
+        )
+        mock_validate_exist.side_effect = (
+            NodeValidationError(validation_errors[1]) if validation_errors[1] else None
+        )
+
+        validator._validate_node_inputs(test_node)
+
+        mock_validate_req.assert_called_once_with(add_node_info, node_element.inputs)
+        mock_validate_exist.assert_called_once_with(add_node_info, node_element.inputs)
+
+    expected_errors = [
+        NodeErrorInfo(node_id="Test Add", message=msg)
+        for msg in validation_errors
+        if msg
+    ]
+
+    assert validator.node_errors == expected_errors
+
+
+def test_workflow_validator_validate_node_inputs_node_type_error(
+    workflow_node_graph: dict[str, Any],
+):
+    """
+    Asserts `_validate_node_inputs` stores correct error message with
+    associated node id if teh node type does not exist.
+    """
+    validator = WorkflowValidator(
+        node_info_list=[],
+        graph=workflow_node_graph,
+    )
+
+    node_id = "Test Add"
+    node_element = NodeElement(**workflow_node_graph["nodes"][node_id])
+    test_node = (node_id, node_element)
+
+    validator._validate_node_inputs(test_node)
+
+    assert validator.node_errors == [
+        NodeErrorInfo(node_id="Test Add", message="The 'TestAdd' node does not exist.")
+    ]
