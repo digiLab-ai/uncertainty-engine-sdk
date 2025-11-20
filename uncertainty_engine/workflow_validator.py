160f47e3b73ab8f390f6fd1bd8c3421db0d62717
natalietf <natalie.tripp.foulds@gmail.com>
2025-11-20 10:03:00 +0000

    correct error message

--- uncertainty_engine/workflow_validator.py
+++ uncertainty_engine/workflow_validator.py
@@ -232,7 +232,7 @@ class WorkflowValidator:
                     RequestedOutputErrorInfo(
                         requested_output_id=output_id,
                         message=(
-                            "Requested output must be a dictionary, not a `Handle` object."
+                            "Requested output must be a dictionary, not a `Handle` object. Did you mean to use `handle.model_dump()`?"
                         ),
                     )
                 )
