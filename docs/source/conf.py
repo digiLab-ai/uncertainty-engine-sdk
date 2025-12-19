2bfa4fd46163e4287e95de118c229c03a41c6c71
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-12-19 12:07:54 +0000

    Add the markdown builder and RTD theme to the conf file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,9 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx_markdown_builder",
+    "sphinx_rtd_theme",
 ]
 
 templates_path = ["_templates"]
@@ -26,5 +29,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
